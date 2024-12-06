5bfedc26c2f7040f411af371362ab84fa5576a0b
jcool-kfujikawa <k.fujikawa@j-cool.fm>
2024-12-06 10:46:26 +0900

    Update archive-news.php

--- src/public/archive-news.php
+++ src/public/archive-news.php
@@ -4,7 +4,7 @@
     <h1 class="c-heading">News</h1>
     <div class="p-news__list">
       <?php
-      $paged = max(1, get_query_var('paged', 1));
+      $paged = max(3, get_query_var('paged', 3));
 
       $news_query = new WP_Query([
         'post_type' => 'news',
